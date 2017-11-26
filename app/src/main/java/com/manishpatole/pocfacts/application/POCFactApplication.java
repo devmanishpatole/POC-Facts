9a18b2562c78cbb0891fde05a34c641fb8915002
Manish Patole <manish.patole.mp@gmail.com>
2017-11-26 21:52:26 +0530

    Added canary leak and completed inspection of application for any memory leaks. Code clean up.

--- app/src/main/java/com/manishpatole/pocfacts/application/POCFactApplication.java
+++ app/src/main/java/com/manishpatole/pocfacts/application/POCFactApplication.java
@@ -2,6 +2,7 @@ package com.manishpatole.pocfacts.application;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
 
@@ -20,6 +21,13 @@ public class POCFactApplication extends Application {
     super.onCreate();
     sPocFactApplication = this;
     initImageCaching();
+
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    LeakCanary.install(this);
   }
 
   private void initImageCaching() {
